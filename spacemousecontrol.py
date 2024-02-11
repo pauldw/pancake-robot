003b81a74cd87fd8e06f0c9497a30f76ce6da6b0
Paul Walker <pwalker@fvml.ca>
2024-02-11 08:18:32 -0500

    Add 2024-02-11 trial notes

--- spacemousecontrol.py
+++ spacemousecontrol.py
@@ -52,7 +52,23 @@ import pyspacemouse
 # Need to wait a bit for batter overflow to fall off ladle into reservoir
 # Need a way to step through sequences and insert/update/delete commands
 # Need a way to verify ladle location as it can move, maybe a short test sequence?
-
+#
+# Fix issue where ladle slides out to the side.
+#   Try tipping the ladle more slowly so the batter comes out and reduces weight.
+#   Try adding a squish rubber end.
+#   Try adding an external "cage" of sorts that
+#   Try shortening ladle arm.
+# Apply more butter.
+# Fix ladle in its location with more certainty (try fixing the measuring cup with a ring at the base, first)
+# Tip batter more into the center of the iron.
+# Push pancake directly towards the bowl (slightly at an angle).
+# Have an infinite loop mode so you don't have to remember to start the next pancake (pancakes should just appear).  Also consider "autonomation" where the robot lets you know the batter is empty.
+# Consider adding a "pancake chute", possibly combined with raising the iron so the pancake slides down the chute.
+# Add some indicator of the passage of time
+#   Have the robot rotate around occasionally.
+#   "Have you tried the hot fudge sunday?"
+#   Have the robot start a 2 minute countdown timer
+# Spatula start scooping (after pushing) closer to edge so the pancake edge isn't squished.
 
 #######################################################
 """
